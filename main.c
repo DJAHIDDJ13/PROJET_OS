b3e8de1ecf01d51b229a760ba7d9c6bab8e03165
Utilisateur <user@debian.arcahe.ovh>
2018-03-29 13:30:20 +0200

    chaged main

--- main.c
+++ main.c
@@ -13,19 +13,10 @@ int main () {
 	initDeckLib();
 	int  file = open("./fich",0);
 	infoJeu info = lireEntree(file);
-	
-	printf(" %d %d %d \n",info.nbrJoueurs,info.nbrDecks,info.nbrMains);
-	
-	for(int i=0;i<info.nbrJoueurs; i++)
-	{
-		printf("%d;%d%c;%d;%d\n",info.joueurs[i].nbrJetons,info.joueurs[i].strategie.mise,info.joueurs[i].strategie.type,info.joueurs[i].valStop,info.joueurs[i].objJetons);
-	}
-		
-	deck_t *deck = initDeck(1, info.nbrDecks);
-	/*shuffleDeck(deck);
+	deck_t* deck = initDeck(1, info.nbrDecks);
+	shuffleDeck(deck);
 	play(info, deck);
-	
-	removeDeck(deck);*/
+	removeDeck(deck);
 	close(file);
 	return(0);
 }
