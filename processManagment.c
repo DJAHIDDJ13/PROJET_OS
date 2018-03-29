80a719ec184649af686265861242f07ceee1f394
Utilisateur <user@debian.arcahe.ovh>
2018-03-29 12:34:16 +0200

    Envoi d'information joueur et boucle d'attent

--- processManagment.c
+++ processManagment.c
@@ -46,30 +46,57 @@ void play(infoJeu info, deck_t *deck){
 			close(ChildPipe[1]);
 			close(ParentPipe[0]);
 			int in = ChildPipe[0], out = ParentPipe[1];
+			//Envoi des information joueur
+			write(out, &info.joueurs[i], sizeof(joueur));
 			//initialisation des cartes joueur
 			pushCard(cartesJoueurs[i], tops+i, deck);
-			//envoi des cartes
-			write(out, tops+i, sizeof(int));
-			write(out, cartesJoueurs[i], sizeof(int) * 22);
-			int sig = 0;
-			read()
+			pushCard(cartesJoueurs[i], tops+i, deck);
+			int sig;
+			do{
+				//Recevoir signal
+				read(in, &sig, sizeof(int));
+				printf("sig = %d\n", sig);
+				if(sig){
+					//piochement
+					pushCard(cartesJoueurs[i], tops+i, deck);
+					//envoi des cartes
+					write(out, tops+i, sizeof(int));
+					write(out, cartesJoueurs[i], sizeof(int) * 22);
+				}
+			} while(sig);
 			while(wait(NULL)>=0);
 		} else {
 			//Initialisation des pipes dans le fils
 			close(ChildPipe[0]);
 			close(ParentPipe[1]);
 			int out = ChildPipe[1], in = ParentPipe[0];
-			//recevoir les cartes 
-			int top;
-			int *cartes;
-			read(in, &top, sizeof(int));
-			read(in, cartes, sizeof(int)*22);
-			printf("[");
-			for(int j=0; j<top; j++){
-				printf("%d%s", cartes[j], j!=top-1?",":"");
+			//Recevoir les information de jeu
+			joueur info;
+			read(in, &info, sizeof(joueur));
+			printf("Max for %d is: %d\n",i, info.valStop);
+			//initialisation de la somme
+			int somme = 0;
+			while(somme <= info.valStop){
+				//Envoi de signal
+				int sig = 1;
+				write(out, &sig, sizeof(int));
+				//recevoir les cartes 
+				int top;
+				int *cartes;
+				read(in, &top, sizeof(int));
+				read(in, cartes, sizeof(int)*22);
+				//Affichage des cartes
+				printf("[");
+				somme = 0;
+				for(int j=0; j<top; j++){
+					printf("(%d,%d)%s",cartes[j], getValueFromCardID(cartes[j])>9?10:getValueFromCardID(cartes[j]), j!=top-1?",":"");
+					somme += getValueFromCardID(cartes[j])>9?10:getValueFromCardID(cartes[j]);
+				}
+				printf("]\n");
 			}
-			printf("]\n");
-			pids[i] = pid;
+			//Envoi de signal d'arret
+			int sig = 0;
+			write(out, &sig, sizeof(int));
 			exit(0);
 		}
 	}
