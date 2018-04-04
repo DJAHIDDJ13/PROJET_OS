e64dba9fd18c845aeab842b2be6b7efbda760dd2
djahid <djahiddj13@github.com>
2018-04-04 07:39:54 +0200

    added bet

--- processManagment.c
+++ processManagment.c
@@ -22,9 +22,16 @@ void initCards(int **cartes, int *tops, int nbrJoueurs){
 void play(infoJeu info, deck_t *deck){
 	int **cartesJoueurs = malloc(sizeof(int*) * info.nbrJoueurs);
 	for(int i=0; i<info.nbrJoueurs; i++)
-		cartesJoueurs[i] = malloc(sizeof(int) * 22); //Le plus pire cas est 22 l'as consecutifs
+		cartesJoueurs[i] = malloc(sizeof(int) * 22); //Le plus pire cas c'est 22 l'as consecutifs
 	int *tops = malloc(sizeof(int) * info.nbrJoueurs);
 	initCards(cartesJoueurs, tops, info.nbrJoueurs);
+	int topBanque = 0;
+	int cartesBanque[22];
+	int sommeBanque = 0;
+	while(sommeBanque < 16){
+		pushCard(cartesBanque, &topBanque, deck);
+		sommeBanque += cartesBanque[topBanque-1];
+	}
 	for(int i=0; i<info.nbrJoueurs; i++){
 		//Creation des pipes
 		int ChildPipe[2];
@@ -52,17 +59,34 @@ void play(infoJeu info, deck_t *deck){
 			pushCard(cartesJoueurs[i], tops+i, deck);
 			int sig;
 			do{
-				//Recevoir signal
-				read(in, &sig, sizeof(int));
-				printf("sig = %d\n", sig);
-				if(sig){
-					//piochement
-					pushCard(cartesJoueurs[i], tops+i, deck);
-					//envoi des cartes
-					write(out, tops+i, sizeof(int));
-					write(out, cartesJoueurs[i], sizeof(int) * 22);
+				do{
+					//Recevoir signal
+					read(in, &sig, sizeof(int));
+					printf("sig = %d\n", sig);
+					if(sig){
+						//piochement
+						pushCard(cartesJoueurs[i], tops+i, deck);
+						//envoi des cartes
+						write(out, tops+i, sizeof(int));
+						write(out, cartesJoueurs[i], sizeof(int) * 22);
+					}
+				} while(sig == 1);
+				//calcul de win
+				int sommeJoueur = 0;
+				for(int j=0; j<tops[i]; j++){
+					sommeJoueur += getValueFromCardID(cartesJoueurs[i][j])>9?10:getValueFromCardID(cartesJoueurs[i][j]);
 				}
-			} while(sig);
+				int win;
+				if(sommeJoueur > 21){
+					win = 0;
+				} else if(sommeJoueur > sommeBanque){
+					win = 1;
+				} else {
+					win = 0;
+				}
+				//envoi de win
+				write(out, &win, sizeof(int));
+			} while(sig != -1);
 			while(wait(NULL)>=0);
 		} else {
 			//Initialisation des pipes dans le fils
@@ -73,29 +97,56 @@ void play(infoJeu info, deck_t *deck){
 			joueur info;
 			read(in, &info, sizeof(joueur));
 			printf("Max for %d is: %d\n",i, info.valStop);
-			//initialisation de la somme
-			int somme = 0;
-			while(somme <= info.valStop){
-				//Envoi de signal
-				int sig = 1;
-				write(out, &sig, sizeof(int));
-				//recevoir les cartes 
-				int top;
-				int cartes[22];
-				read(in, &top, sizeof(int));
-				read(in, cartes, sizeof(int)*22);
-				//Affichage des cartes et calcul de somme
-				printf("[");
-				somme = 0;
-				for(int j=0; j<top; j++){
-					printf("(%d,%d)%s",cartes[j], getValueFromCardID(cartes[j])>9?10:getValueFromCardID(cartes[j]), j!=top-1?",":"");
-					somme += getValueFromCardID(cartes[j])>9?10:getValueFromCardID(cartes[j]);
+			int mise = info.strategie.mise;
+			while(info.nbrJetons - mise < 0 && info.nbrJetons < info.objJetons){
+				info.nbrJetons -= mise;
+				//initialisation de la somme
+				int somme = 0;
+				while(somme <= info.valStop){
+					//Envoi de signal
+					int sig = 1;
+					write(out, &sig, sizeof(int));
+					//recevoir les cartes 
+					int top;
+					int cartes[22];
+					read(in, &top, sizeof(int));
+					read(in, cartes, sizeof(int)*22);
+					//Affichage des cartes et calcul de somme
+					printf("[");
+					somme = 0;
+					for(int j=0; j<top; j++){
+						printf("(%d,%d)%s",cartes[j], getValueFromCardID(cartes[j])>9?10:getValueFromCardID(cartes[j]), j!=top-1?",":"");
+						somme += getValueFromCardID(cartes[j])>9?10:getValueFromCardID(cartes[j]);
+					}
+					printf("]\n");
 				}
-				printf("]\n");
+				//Envoi de signal d'arret de partie
+				int sig = 0;
+				write(out, &sig, sizeof(int));
+				//Recevoir le win
+				int win;
+				read(in, &win, sizeof(int));
+				if(win){
+					info.nbrJetons += mise;
+					mise = info.strategie.mise;
+				} else {
+					switch(info.strategie.type){
+						case '*':
+							mise = info.nbrJetons;
+							
+							break;
+						case '+':
+							mise *= 2;
+							break;
+						case '-':
+							mise /= 2;
+					}
+				}
 			}
 			//Envoi de signal d'arret
-			int sig = 0;
+			int sig = -1;
 			write(out, &sig, sizeof(int));
+
 			exit(0);
 		}
 	}
